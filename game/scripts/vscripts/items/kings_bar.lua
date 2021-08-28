5608bac2bf36da86fd17c09533dd2bf3381cf083
DimabroStins <garkudhad@gmail.com>
2021-08-28 21:19:33 +0300

    Patch 6.9.1

--- game/scripts/vscripts/items/kings_bar.lua
+++ game/scripts/vscripts/items/kings_bar.lua
@@ -2,8 +2,7 @@ function KingsBar_purge(keys)
 	local ability = keys.ability
 	local caster = keys.caster
 	
-	caster:Purge( false, true, false, true, true )
-	caster:RemoveModifierByName("modifier_slark_pounce_leash")
+	caster:Purge( false, true, false, false, false )
 
 	caster:EmitSound("DOTA_Item.BlackKingBar.Activate")
 end
